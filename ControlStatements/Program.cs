ff0758e5f012ecdd5e4e4334d90d0d8922530f52
Melek <115369650+melekakkoyunlu@users.noreply.github.com>
2024-03-16 20:47:16 +0300

    Add files via upload

---
+++ ControlStatements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your nickname");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your password");
+            string password = Console.ReadLine();
+
+            if (name=="admin" && password=="1234")
+            {
+               Console.WriteLine("Welcome");
+           
+            }
+            else
+            {
+                Console.WriteLine("Invalid password or nickname");
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ DataTypesAndVariables/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypesAndVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          ///*  int numberOne = 10 ,
+          //      numberTwo = 40 , 
+          //      result = numberOne + numberTwo ;
+            
+          //  Console.WriteLine(result);
+          //*/
+
+
+          //  Console.WriteLine(Byte.MaxValue +" " +Byte.MinValue);
+          //  Console.WriteLine(SByte.MaxValue + " " + SByte.MinValue);
+          //  Console.WriteLine(Int16.MaxValue + " " + Int16.MinValue);  //short
+          //  Console.WriteLine(UInt16.MaxValue + " " + UInt16.MinValue);
+          //  Console.WriteLine(Int32.MaxValue + " " + Int32.MinValue);  //int
+          //  Console.WriteLine(UInt16.MaxValue + " " + UInt16.MinValue);
+          //  Console.WriteLine(Int64.MaxValue + " " + Int64.MinValue);  //long
+          //  Console.WriteLine(UInt64.MaxValue + " " + UInt64.MinValue);
+
+
+
+
+
+            Console.ReadLine();
+
+            
+        }
+    }
+}
